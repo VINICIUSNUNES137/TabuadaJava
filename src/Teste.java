65a3dd696993039ad6cbdbe1839b3d97bb0ede04
VINICIUSNUNES137 <90266473+VINICIUSNUNES137@users.noreply.github.com>
2022-09-18 21:49:06 -0300

    18-09

---
+++ src/Teste.java
@@ -0,0 +1,65 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import javax.swing.JRadioButton;
+
+public class Teste {
+
+	private JFrame frame;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Teste window = new Teste();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Teste() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel teste = new JPanel();
+		frame.getContentPane().add(teste, BorderLayout.NORTH);
+		
+		JButton btnNewButton = new JButton("teste");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		frame.getContentPane().add(btnNewButton, BorderLayout.WEST);
+		
+		table = new JTable();
+		frame.getContentPane().add(table, BorderLayout.CENTER);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("New radio button");
+		frame.getContentPane().add(rdbtnNewRadioButton, BorderLayout.SOUTH);
+	}
+
+}
